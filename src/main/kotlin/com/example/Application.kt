8951f45f03e9cd90e922e68acce0dc520d7773f6
tadashi izuno <tadashi.izuno@gmail.com>
2020-11-21 10:10:45 +0900

    update

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Gradle5KotlinApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<Gradle5KotlinApplication>(*args)
+	runApplication<Application>(*args)
 }
